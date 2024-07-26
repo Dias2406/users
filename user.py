1a156916bc1905f13484960ffca98155b86c182f
Dias2406 <ucabdj5@ucl.ac.uk>
2024-07-26 13:39:33 +0100

    Test

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 # user.py
 import uuid
-
+from .utils.utils import validate_email
 """
 Contains the User class which uses the uuid library to generate unique user IDs.
 """
@@ -14,4 +14,6 @@ class User:
         return f"User [ID: {self.id}, Name: {self.name}, Email: {self.email}]"
     
     def update_email(self, new_email):
+        if validate_email(new_email):
+            raise ValueError("Invalid email format")
         self.email = new_email
